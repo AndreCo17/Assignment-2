d8d3e8ba2b4fe028afa85fb68c4f1ca198051dcd
Jean Provost <jprov310@mtroyal.ca>
2020-12-11 19:50:33 -0700

    favorites changed

--- WEB2-ASG2/favorites.php
+++ WEB2-ASG2/favorites.php
@@ -1,5 +1,5 @@
 <?php 
-session_start();
+/* session_start();
 require_once '../php/api/config.inc.php';
 require_once '../php/api/ASG2-classes.php';
 include '../php/nav-header.php'
@@ -40,5 +40,79 @@ if ($eventURL == "favJA"){
 } else {
             $imageSRC = 'favNEE';
       }
-//https://stackoverflow.com/questions/22621357/how-to-store-favorites-using-session
+//https://stackoverflow.com/questions/22621357/how-to-store-favorites-using-session */
+require_once './api/config.inc.php';
+session_start();
+if(isset($_SESSION['loggin']) && $_SESSION['loggin'] == true) {
+   $userID = $_SESSION['id'];
+}
+
+
+
+try {
+   $pdo = new PDO(DBCONNSTRING, DBUSER, DBPASS);
+   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      
+      $sql = "CREATE TABLE IF NOT EXISTS customer_fav (
+         CustomerID INTEGER(50),
+         PaintingID INTEGER(100),
+         Title VARCHAR(255),
+         PRIMARY KEY (CustomerID)
+      ) ENGINE=innodb DEFAULT CHARSET=utf8";
+      $pdo->exec($sql); // found from https://www.tutorialrepublic.com/php-tutorial/php-mysql-create-table.php
+   //}  
+      $sql = "SELECT *  from customer_fav INNER JOIN Paintings ON customer_fav.PaintingID = Paintings.PaintingID WHERE CustomerID= '".$userID."'";
+      $result = $pdo->query($sql);
+      $favorites = $result->fetchAll(PDO::FETCH_ASSOC);
+      
+      foreach($favorites as $row) {
+         $fileName = "images/paintings/square/" . $row['ImageFileName'];
+         echo '<ul>';
+         echo '<li>';
+         echo '<a href="single-painting.php?PaintingID="'.$row['PaintingID'].'"">';
+         echo "'<img src='" .$fileName.".jpg' width='100'>'"; 
+         echo '<form method="post">';
+         echo '<input type="submit" name="single" id="single">';
+         echo '</form>';
+         echo '</a>';
+         echo '</li>';
+         echo '</ul>';
+      }
+
+      if(array_key_exists('single', $_POST)) { //similar code from https://stackoverflow.com/questions/32824360/run-php-function-on-button-click
+         removeSingle($userID, $pdo, $row['PaintingID']);
+      }
+
+      if(array_key_exists('all', $_POST)) {
+         removeAll($userID, $pdo);
+      }
+   
+     
+
+}catch (Exception $e) {
+   die( $e->getMessage());
+}
+echo '<form method="post">';
+echo '<input type="submit" name="all" id="all" value="Remove All Favorites">';
+echo '</form>';
+
+function addToFavorites($pdo, $userID,  $paintingID, $title) {
+   $sql = "INSERT INTO customer_fav (CustomerID, PaintingID, Title) VALUES (':CustomerID', ':PaintingID', ':Title')";
+   $statement = $pdo->prepare($sql);
+   $statement->bindValue(':CustomerID', $userID);
+   $statement->bindValue(':PaintingID', $paintingID);
+   $statement->bindValue(':Title', $title);
+   $statement->execute();
+}
+
+function removeSingle($userID, $pdo, $paintingID) {
+   $sql = "DELETE from customer_fav WHERE CustomerID='".$userID."' AND PaintingID='".$paintingID."'"; //similar code from https://www.w3schools.com/php/php_mysql_delete.asp
+   $pdo->exec($sql);
+}
+
+function removeAll($userID, $pdo) {
+   $sql = "DELETE from customer_fav WHERE CustomerID='".$userID."'";
+   $pdo->exec($sql);
+}
+
 ?>
